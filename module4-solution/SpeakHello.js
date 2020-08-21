15fe8f79ddd38f497068771d87f6cccc24dea282
Masoud <msoolymani@gmail.com>
2020-08-21 20:26:35 +0100

    completed mudole-4 assignments

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,10 @@
+// External library - SpeakHello
+
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
